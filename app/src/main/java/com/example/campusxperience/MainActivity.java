ca22210bf1d2c2b9be016bcdc691b1d7e81b8c91
Nathan Rener Malagapo <nathanrener.malagapo@cit.edu>
2024-05-28 19:47:17 +0800

    first commit

--- app/src/main/java/com/example/campusxperience/MainActivity.java
+++ app/src/main/java/com/example/campusxperience/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.campusxperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openButton = (Button) findViewById(R.id.open); // Changed from "-" to "="
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent); // Added startActivity to actually open the Home activity
+
+                Toast.makeText(MainActivity.this, "CampusXperience", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

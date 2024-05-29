7fc2aee28ab6403be2d15a13f10ebcc8eb768526
Nathan Rener Malagapo <nathanrener.malagapo@cit.edu>
2024-05-29 16:11:15 +0800

    added new files - rsvp1,2,3,4

--- app/src/main/java/com/example/campusxperience/rsvpTicketing.java
+++ app/src/main/java/com/example/campusxperience/rsvpTicketing.java
@@ -1,6 +1,9 @@
 package com.example.campusxperience;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class rsvpTicketing extends AppCompatActivity {
 
+    Button rsvp1;
+    Button rsvp2;
+    Button rsvp3;
+    Button rsvp4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,43 @@ public class rsvpTicketing extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        rsvp1 = findViewById(R.id.rsvp1);
+        rsvp2 = findViewById(R.id.rsvp2);
+        rsvp3 = findViewById(R.id.rsvp3);
+        rsvp4 = findViewById(R.id.rsvp4);
+
+        rsvp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(rsvpTicketing.this, rsvp1.class);
+                startActivity(intent);
+            }
+        });
+
+        rsvp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(rsvpTicketing.this, rsvp2.class);
+                startActivity(intent);
+            }
+        });
+
+        rsvp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(rsvpTicketing.this, rsvp3.class);
+                startActivity(intent);
+            }
+        });
+
+        rsvp4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(rsvpTicketing.this, rsvp4.class);
+                startActivity(intent);
+            }
         });
     }
 }
